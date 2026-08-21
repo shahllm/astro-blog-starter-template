7a24eaa4006345eea2f3508411f0942872899db4
shahllm <shahumass@gmail.com>
2026-08-20 18:31:34 -0600

    Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  site: 'https://test.shahflare.win',
   output: 'server',
   adapter: cloudflare(),
 });
